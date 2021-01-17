766cccbd34cef5c50749ca6a41a141e02befa2b9
Yonatan Melka <ybmelka@gmail.com>
2021-01-17 16:19:47 -0600

    add bubble sort animation

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,26 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import bubbleSort from '../algorithms/bubbleSort';
 
 const NUM_OF_BARS = 100;
 const MIN_HEIGHT = 10;
 const MAX_HEIGHT = 500;
 
 function Board() {
-  const randomArray = [];
+  const [barArray, setBarArray] = useState([]);
 
-  for (let i = 0; i < NUM_OF_BARS; i++) {
-    const randHeight = Math.floor(Math.random() * (MAX_HEIGHT - MIN_HEIGHT + 1)) + MIN_HEIGHT;
-    randomArray.push(randHeight);
-  }
+  useEffect(() => {
+    const newArray = [];
+    for (let i = 0; i < NUM_OF_BARS; i++) {
+      const randHeight = Math.floor(Math.random() * (MAX_HEIGHT - MIN_HEIGHT + 1)) + MIN_HEIGHT;
+      newArray.push(randHeight);
+    }
+    setBarArray(newArray);
+  }, []);
+
+  const handleBubbleSort = () => {
+    const animation = bubbleSort(barArray);
+    for (let i = 0; i < animation.length; i++) {
+      const barDivs = document.querySelectorAll('.array-bar');
+      const [barOne, barTwo] = animation[i];
+      setTimeout(() => {
+        const temp = barDivs[barOne].style.height;
+        barDivs[barOne].style.height = barDivs[barTwo].style.height;
+        barDivs[barTwo].style.height = temp;
+        barDivs[barOne].style.backgroundColor = 'red';
+        barDivs[barTwo].style.backgroundColor = 'blue';
+        setTimeout(() => {
+          barDivs[barOne].style.backgroundColor = 'black';
+          barDivs[barTwo].style.backgroundColor = 'black';
+        }, 50);
+      }, i * 50);
+    }
+  };
 
   return (
     <div
-      style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end', marginTop: 80 }}
+      style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
     >
-      {randomArray.map((height, idx) => {
-        return (
-          <div key={idx} style={{ height, width: 10, backgroundColor: 'black', margin: 2 }}></div>
-        );
-      })}
+      <button onClick={handleBubbleSort}>bubbleSort</button>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'flex-end',
+          marginBottom: 100,
+        }}
+      >
+        {barArray.map((height, idx) => {
+          return (
+            <div
+              key={idx}
+              className='array-bar'
+              style={{ height, width: 10, backgroundColor: 'black', margin: 1 }}
+            ></div>
+          );
+        })}
+      </div>
     </div>
   );
 }
